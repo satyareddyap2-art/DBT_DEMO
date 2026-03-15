5be5dfc899cfc2778de28cace116a9a3c42f3310
Satya Reddy <satya.reddy@nz.ey.com>
2026-03-15 15:09:04 +1300

    updated station_dimension table

--- analytics/models/example/station_dimension.sql
+++ analytics/models/example/station_dimension.sql
@@ -5,6 +5,7 @@ with bike as (
     start_station_name as station_name,
     start_lat as station_lat,
     start_lng as station_lng   
-    from {{source('demo','bike')}}
+    from {{source('demo','bike')}} 
+    where ride_id != 'ride_id'
 ) 
 select * from bike
